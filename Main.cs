427e5edb736b090edbb156488d672ad8dfc851ad
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2021-02-06 18:57:57 +0800

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShadowOfWorld_Console_CSharp {
+  class Start {
+    static void Main(string[] args) {
+      Graphic.Start.Core();
+
+      
+
+    }
+  }
+}
+
+namespace Graphic {
+  class Start {
+    public static void Core() {
+      Logo();
+      Thread.Sleep(2000);
+      Console.Clear();
+    }
+    private static void Logo() {
+      Console.WriteLine("\n\n\n\n       == ===        =         =     ======       ==     ==     ======     =========     \n       ==    ==      ==       ==     ==    ==     ==     ==   ===          =   =   =     \n       ==     =      == =   = ==     ==     ==    ==     ==    ===             =         \n       ==  ==        ==  = =  ==     ==     ==    ==     ==       ====         =         \n       ==   ==       ==   =   ==     ==     ==    ==     ==           ==       =         \n       ==    ==      ==       ==     ==    =      ==     ==            =       =         \n       ==     ===    ==       ==    === ===       =========    =======        ===        \n");
+    }
+  }
+
+}
